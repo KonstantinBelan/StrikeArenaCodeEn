b15033d296f670fdf78f54c4cbb5886bd695522d
KonstantinBelan <95037979+KonstantinBelan@users.noreply.github.com>
2024-11-16 03:08:25 +0700

    Update and rename script2.js to script2-v1.js

--- script2-v1.js
+++ script2-v1.js
@@ -7,55 +7,55 @@ $(document).ready(function() {
                       text  : item,
                       value : item
                     }).attr('data-calc-value', value))),
-                    this.prepend('<option value="" selected disabled>Укажите город</option>')
+                    this.prepend('<option value="" selected disabled>Choose your city</option>')
                 ];
             };
         })(jQuery);
         
         var belarus = {
-          Минск: 2,
-          Гомель: 2,
-          Другое : 2,
+          Minsk: 2,
+          Gomel: 2,
+          Other : 2,
         };
         var kazahstan = {
-            Актобе: 2,
-            Актау: 2,
-            Алматы: 2,
-            Атырау: 2,
-            Астана: 2,
-            Караганда: 2,
-            Кокшетау: 2,
-            Костанай: 2,
-            Кызылорда: 2,
-            Петропавловск: 2,
-            Семей: 2,
-            Тараз: 2,
-            Туркестан: 2,
-            УстьКаменогорск: 2,
-            Уральск: 2,
-            Шымкент: 2,
-            Экибастуз: 2,
-            Другое: 2,
+Aktobe: 2,
+Aktau: 2,
+Almaty: 2,
+Atyrau: 2,
+Astana: 2,
+Karaganda: 2,
+Kokshetau: 2,
+Kostanay: 2,
+Kyzylorda: 2,
+Petropavlovsk: 2,
+Semey: 2,
+Taraz: 2,
+Turkestan: 2,
+Ustkamenogorsk: 2,
+Uralsk: 2,
+Shymkent: 2,
+Ekibastuz: 2,
+Other: 2,
         };
         var uzbekistan = {
-          Ташкент: 2,
-          Наманган: 2,
-          Самарканд: 2,
-          Андижан: 2,
-          Другое: 2,
+Tashkent: 2,
+Namangan: 2,
+Samarkand: 2,
+Andijan: 2,
+Other: 2,
         };
         var oae = {
-          Дубай: 3,
-          Другое: 3,
+Dubai: 3,
+Other: 3,
         };
         var argentina = {
-          БуэносАйрес: 3,
-          Кордова: 3,
-          Росарио: 3,
-          Мендоса: 3,
-          ЛаПлата: 3,
-          Сальта: 3,
-          Другое: 3,
+BuenosAires: 3,
+Cordoba: 3,
+Rosario: 3,
+Mendoza: 3,
+LaPlata: 3,
+Salta: 3,
+Other: 3,
         };
 
         
@@ -65,37 +65,37 @@ $(document).ready(function() {
             $('.country_btn .tn-atom[href="/"]').removeClass('selected')
             let params = new URLSearchParams(window.location.search);
             let myCountry = String(params.get('country'));
-            $('.country_btn .tn-atom[href="/?country=' + myCountry + '"]').addClass('selected')
+            $('.country_btn .tn-atom[href="/en/?country=' + myCountry + '"]').addClass('selected')
             if( myCountry == 'belarus' ) {
-                $('.head_country .tn-atom a').text('Беларусь');
+                $('.head_country .tn-atom a').text('Belarus');
                 $('select[name="GOROD"], select[name="city"]').empty().populate(belarus);
                 $('.t-input-phonemask__options-item[data-phonemask-name="Belarus (Беларусь)"]').each(function() {
                   $( this ).click();
                 });
             }
             if( myCountry == 'kazahstan' ) {
-                $('.head_country .tn-atom a').text('Казахстан');
+                $('.head_country .tn-atom a').text('Kazakhstan');
                 $('select[name="GOROD"], select[name="city"]').empty().populate(kazahstan);
                 $('.t-input-phonemask__options-item[data-phonemask-name="Kazakhstan (Казахстан)"]').each(function() {
                   $( this ).click();
                 });
             }
             if( myCountry == 'uzbekistan' ) {
-                $('.head_country .tn-atom a').text('Узбекистан');
+                $('.head_country .tn-atom a').text('Uzbekistan');
                 $('select[name="GOROD"], select[name="city"]').empty().populate(uzbekistan);
                 $('.t-input-phonemask__options-item[data-phonemask-name="Uzbekistan (Oʻzbekiston)"]').each(function() {
                   $( this ).click();
                 });
             }
             if( myCountry == 'oae' ) {
-                $('.head_country .tn-atom a').text('ОАЭ');
+                $('.head_country .tn-atom a').text('UAE');
                 $('select[name="GOROD"], select[name="city"]').empty().populate(oae);
                 $('.t-input-phonemask__options-item[data-phonemask-name="United Arab Emirates (الإمارات العربية المتحدة)"]').each(function() {
                   $( this ).click();
                 });
             }
             if( myCountry == 'argentina' ) {
-                $('.head_country .tn-atom a').text('Аргентина');
+                $('.head_country .tn-atom a').text('Argentina');
                 $('select[name="GOROD"], select[name="city"]').empty().populate(argentina);
                 $('.t-input-phonemask__options-item[data-phonemask-name="Argentina"]').each(function() {
                   $( this ).click();
